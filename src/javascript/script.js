dc51eb9ffad0c18a43fb4be2b4c06ef3849e6268
ativa360pro <ativa360pro@gmail.com>
2025-03-11 22:35:07 -0300

    Update script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,28 +1,36 @@
 $(document).ready(function() {
-   
-    $('#mobile_btn').on('click touchstart', function () {
+    $('#mobile_btn').on('click touchstart', function (e) {
+        e.preventDefault();
         $('#mobile_menu').toggleClass('active');
         $(this).find('i').toggleClass('fa-x'); 
     });
 
-    $('.nav-item').on('click', function () {
+
+    $('.nav-item a').on('click', function () {
         $('.nav-item').removeClass('active');
-        $(this).addClass('active');
+        $(this).parent().addClass('active'); 
         $('#mobile_menu').removeClass('active');
     });
+
 
     $('.btn-acesso').on('click', function (e) {
         e.preventDefault();
         window.open("https://pay.kiwify.com.br/QmmcbQF", "_blank");
     });
 
-    $(".faq-question").on("click", function () {
+
+    $(".faq-question").on("click touchstart", function (e) {
+        e.preventDefault();
         let answer = $(this).next(".faq-answer");
-    
-       
+
+        if (answer.is(":visible")) {
             answer.slideUp();
+            $(this).removeClass('active'); 
         } else {
             $(".faq-answer").slideUp(); 
+            $(".faq-question").removeClass('active'); 
             answer.slideDown();
+            $(this).addClass('active');
         }
     });
+});
